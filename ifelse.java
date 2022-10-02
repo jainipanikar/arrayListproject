3ddb08cb5596a1855f3c2ef6c58b026623591898
jainipanikar <jainipanikar@gmail.com>
2022-10-02 13:12:54 -0400

    first commit

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+package arrayListwarmup;
+
+public class ifelse {
+	
+	public static void main(String[] args) {
+		
+	int gas = 100;
+	if (gas < 25) {
+		
+		System.out.println( "gas is low");
+		
+	} else if ( gas > 50) {
+		
+		System.out.print( " gas is avarage");
+	}
+	else {
+		System.out.println(" gas is high");
+	}
+	}
+	}
+
+
